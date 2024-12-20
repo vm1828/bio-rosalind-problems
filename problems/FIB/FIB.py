3dba4b927fb97867b0a06ff3b2340e41d48b8d3d
vm1828 <vm18281828@gmail.com>
2024-12-20 16:14:57 +0200

    Add FIB

--- problems/FIB/FIB.py
+++ problems/FIB/FIB.py
@@ -3,13 +3,30 @@
 from shared.constants import PROBLEM, SOLUTION
 
 
-def fib_soln():
-    ...
+def fib_soln(n: int, k: int = 1) -> int:
+    """Compute the nth Fibonacci number.
+
+    Args:
+        n (int): Number of days
+        k (int, optional): Reproduction factor (offsprings multiplier). Defaults to 1.
+
+    Returns:
+        int: nth Fibonacci number
+    """
+    a = 1
+    b = 1
+    print(f'Month 1: {a} pair')
+    print(f'Month 2: {b} pair')
+    for i in range(3, n+1):
+        a, b = b, a*k+b
+        print(f'Month {i}: {b} pairs')
+    return b
 
 
 if __name__ == '__main__':
     with open(PROBLEM) as f:
-        s = f.read()
-    solution = fib_soln(s)
+        n, k = f.readline().strip().split(' ')
+        n, k = int(n), int(k)
+    solution = fib_soln(n, k)
     with open(SOLUTION, 'w') as f:
         f.write(solution)
--- problems/FIB/test_FIB.py
+++ problems/FIB/test_FIB.py
@@ -1,7 +1,8 @@
 from problems.FIB.FIB import fib_soln
 
-test_input = ""
-test_output = ""
+test_input = (5, 3)
+test_output = 19
 
-# def test_fib_soln():
-#     assert fib_soln(test_input) == test_output
+
+def test_fib_soln():
+    assert fib_soln(*test_input) == test_output
