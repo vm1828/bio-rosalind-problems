047b2facb4e02721bccbb43c8960c105fad8f051
vm1828 <vm18281828@gmail.com>
2024-12-20 20:43:03 +0200

    Add HAMM

--- problems/HAMM/HAMM.py
+++ problems/HAMM/HAMM.py
@@ -1,14 +1,28 @@
 """Counting Point Mutations"""
 
 from shared.constants import PROBLEM, SOLUTION
-    
-def hamm_soln():
-    ...
+
+
+def hamm_soln(filename: str) -> int:
+    with open(filename) as f:
+        s1 = f.readline().strip()
+        s2 = f.readline().strip()
+        assert len(s1) == len(s2)
+    return sum(ch1 != ch2 for ch1, ch2 in zip(s1, s2))
+
+# def hamm_soln(filename: str) -> int:
+#   with open(filename) as f:
+#     s1 = f.readline().strip()
+#     s2 = f.readline().strip()
+#     assert len(s1) == len(s2)
+#   count = 0
+#   for i in range(len(s1)):
+#     if s1[i] != s2[i]:
+#       count += 1
+#   return count
+
 
 if __name__ == '__main__':
-    with open(PROBLEM) as f:
-        s = f.read()
-    solution = hamm_soln(s)
+    solution = str(hamm_soln(PROBLEM))
     with open(SOLUTION, 'w') as f:
         f.write(solution)
-
--- problems/HAMM/test_HAMM.py
+++ problems/HAMM/test_HAMM.py
@@ -1,8 +1,11 @@
 from problems.HAMM.HAMM import hamm_soln
+from shared.testing_utils import run_with_tmp_file
 
-test_input = ""
-test_output = ""
+test_input = """GAGCCTACTAACGGGAT
+CATCGTAATGACGGCCT
+"""
+test_output = 7
 
-# def test_hamm_soln():
-#     assert hamm_soln(test_input) == test_output
 
+def test_hamm_soln():
+    assert run_with_tmp_file(hamm_soln, test_input) == test_output
