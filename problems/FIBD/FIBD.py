825b9194f0f05a49562b51e3702e7fc3baffdde7
vm1828 <vm18281828@gmail.com>
2024-12-22 17:12:51 +0200

    Add FIBD

--- problems/FIBD/FIBD.py
+++ problems/FIBD/FIBD.py
@@ -6,18 +6,45 @@ from shared.solution import Solution
 class FIBDSolution(Solution):
 
     @staticmethod
-    def algorithm(s: str):
-        """...
+    def algorithm(n: int, m: int) -> int:
+        """Simulates the growth of a rabbit population with aging and mortality constraints.
 
         Args:
-            s (str): ...
+            months (int): The total number of months.
+            m (int): The lifespan of a rabbit in months.
 
         Returns:
-            ...: ...
+            int: The total number of rabbit pairs alive at the end of month n.
         """
-        ...
+        newborn, mature = 1, 0  # Initial state: 1 newborn pair, 0 mature pairs
+        queue = [1]  # Track newborns
+        for _ in range(2, n+1):
+            new_newborn = mature  # Newborns are current mature rabbits
+            dead = queue.pop(0) if len(queue) >= m else 0  # Died this month
+            mature = mature + newborn - dead  # Update mature
+            queue.append(new_newborn)  # Track newborns
+            newborn = new_newborn
+        return newborn + mature
+
+    def _parse(self) -> tuple[int]:
+        with open(self._input_file) as f:
+            n, m = f.readline().strip().split(' ')
+        return int(n), int(m)
+
+    def _solve(self) -> str:
+        return str(self.algorithm(*self._parsed_data))
+
+
+# def fibd(n, m):
+#     seq = [(1, 0), (0, 1)]    # (newborn, mature)
+#     for i in range(2, n):
+#         newborn = seq[i-1][1]
+#         dead = seq[i-m][0] if len(seq) >= m else 0
+#         mature = sum(seq[i-1]) - dead
+#         seq.append((newborn, mature))
+#         print(f'Month {i+1}: {sum(seq[i])} pairs')
+#     return sum(seq[-1])
 
 
 if __name__ == '__main__':
     FIBDSolution()
-
--- problems/FIBD/test_FIBD.py
+++ problems/FIBD/test_FIBD.py
@@ -2,10 +2,9 @@ from problems.FIBD.FIBD import FIBDSolution
 from shared.testing_utils import solution_output
 
 
-test_input = ""
-expected_output = ""
+test_input = "6 3"
+expected_output = "4"
 
 
-# def test_FIBDSolution():
-#     assert solution_output(FIBDSolution, test_input) == expected_output
-
+def test_FIBDSolution():
+    assert solution_output(FIBDSolution, test_input) == expected_output
