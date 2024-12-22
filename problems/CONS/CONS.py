db74247d76592f74ee04970e829cd250c1707cf0
vm1828 <vm18281828@gmail.com>
2024-12-22 14:01:34 +0200

    Add CONS

--- problems/CONS/CONS.py
+++ problems/CONS/CONS.py
@@ -1,23 +1,81 @@
 """Consensus and Profile"""
 
+from collections import defaultdict
+
+import numpy as np
+import pandas as pd
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+
 from shared.solution import Solution
 
 
 class CONSSolution(Solution):
 
     @staticmethod
-    def algorithm(s: str):
-        """...
+    def algorithm(profile: pd.DataFrame) -> str:
+        """
+        Generates a consensus sequence from a profile matrix.
+
+        To generate the profile matrix, the `profile_matrix` method can be used.
 
         Args:
-            s (str): ...
+            profile (pd.DataFrame): A DataFrame representing the profile matrix, 
+                                    where each row corresponds to a nucleotide position 
+                                    and each column represents a nucleotide (A, C, G, T).
 
         Returns:
-            ...: ...
+            str: The consensus sequence, formed by selecting the most frequent nucleotide 
+                at each position in the profile matrix.
         """
-        ...
+        consensus = ''.join([b for b in profile.idxmax(axis=1)])
+        return consensus
+
+    @staticmethod
+    def profile_matrix(records: iter[SeqRecord]) -> pd.DataFrame:
+        """
+        Creates a profile matrix based on multiple sequence alignments.
+
+        Args:
+            records (iter[SeqRecord]): An iterator over SeqRecord objects, where each
+                                    contains a sequence (string) to be used in the profile.
+
+        Returns:
+            pd.DataFrame: A pandas DataFrame representing the profile matrix, where each
+                        column corresponds to a position in the sequence alignment,
+                        and each row corresponds to a nucleotide (A, C, G, T) with
+                        counts of occurrences at that position.
+        Raises:
+            ValueError: If sequences have differing lengths.
+        """
+        # create profile
+        profile = None
+        for record in records:
+            sequence = record.seq
+            if not profile:
+                sequence_length = len(sequence)
+                profile = defaultdict(
+                    lambda: np.zeros(sequence_length, dtype=int))
+            if len(sequence) != sequence_length:
+                raise ValueError('All sequences should have the same length')
+            for i in range(len(sequence)):
+                profile[sequence[i]][i] += 1
+        profile = pd.DataFrame.from_dict(profile)
+        return profile
+
+    def _parse(self) -> iter[SeqRecord]:
+        records = SeqIO.parse(self._input_file, 'fasta')
+        return records
+
+    def _solve(self) -> str:
+        profile = self.profile_matrix(self._parsed_data)
+        consensus = self.algorithm(profile)
+        return f"""{consensus}
+A: {' '.join(profile['A'].astype(str))}
+C: {' '.join(profile['C'].astype(str))}
+G: {' '.join(profile['G'].astype(str))}
+T: {' '.join(profile['T'].astype(str))}"""
 
 
 if __name__ == '__main__':
     CONSSolution()
-
--- problems/CONS/test_CONS.py
+++ problems/CONS/test_CONS.py
@@ -2,9 +2,26 @@ from problems.CONS.CONS import CONSSolution
 from shared.testing_utils import solution_output
 
 
-test_input = ""
-expected_output = ""
+test_input = """>Rosalind_1
+ATCCAGCT
+>Rosalind_2
+GGGCAACT
+>Rosalind_3
+ATGGATCT
+>Rosalind_4
+AAGCAACC
+>Rosalind_5
+TTGGAACT
+>Rosalind_6
+ATGCCATT
+>Rosalind_7
+ATGGCACT"""
+expected_output = """ATGCAACT
+A: 5 1 0 0 5 5 0 0
+C: 0 0 1 4 2 0 6 1
+G: 1 1 6 3 0 1 0 0
+T: 1 5 0 0 0 1 1 6"""
 
 
-# def test_CONSSolution():
-#     assert solution_output(CONSSolution, test_input) == expected_output
+def test_CONSSolution():
+    assert solution_output(CONSSolution, test_input) == expected_output
