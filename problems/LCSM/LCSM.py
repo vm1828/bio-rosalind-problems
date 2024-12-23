97f897ff25b8ba6542d22f223ddbde19e869b9c7
vm1828 <vm18281828@gmail.com>
2024-12-23 12:47:28 +0200

    Add LCSM

--- problems/LCSM/LCSM.py
+++ problems/LCSM/LCSM.py
@@ -1,23 +1,65 @@
 """Finding a Shared Motif"""
 
+import re
 from shared.solution import Solution
 
 
 class LCSMSolution(Solution):
 
     @staticmethod
-    def algorithm(s: str):
-        """...
+    def _shared_motif(seq_0: str, seqs: list[str], start: int, length: int) -> tuple[int, str]:
+        """Find the first motif of a given length in the first sequence that is present in all other sequences."""
+        for i in range(start, len(seq_0)-length+1):
+            motif = seq_0[i:i+length]
+            if all(motif in seq for seq in seqs):
+                return (i, motif)
+        return (-1, '')
+
+    @staticmethod
+    def algorithm(seqs: list[str]) -> str:
+        """
+        Find the longest common motif (substring) across multiple sequences (strings) using binary search.
 
         Args:
-            s (str): ...
+            seqs (list[str]): A list of strings to find the longest common substring.
 
         Returns:
-            ...: ...
+            str: The longest common substring found across all the strings.
         """
-        ...
+        seq_0 = seqs.pop()
+        l, r = 0, len(seq_0)
+        start = 0
+        longest_motif = ''
+        while l < r:
+            m = (l + r) // 2
+            i, motif = __class__._shared_motif(seq_0, seqs, start, m)
+            if i > -1:  # Motif found
+                start = i
+                longest_motif = motif
+                l = m + 1  # Search in the larger half
+            else:
+                r = m  # Search in the smaller half
+        return longest_motif
+
+    def _parse(self) -> list[str]:
+        pattern = re.compile(r'>Rosalind_\d+\s*([ACGT\s]+)')
+        with open(self._input_file) as f:
+            seqs = pattern.findall(f.read().replace("\n", ""))
+        return seqs
+
+    def _solve(self) -> str:
+        return self.algorithm(self._parsed_data)
+
+
+# def find_shared_motif_naive(seqs: list[str]) -> str:
+#     shortest_seq = min(seqs, key=len)
+#     for window_len in range(len(shortest_seq), 0, -1):
+#         for i in range(len(shortest_seq)-window_len+1):
+#             motif = shortest_seq[i:i+window_len]
+#             if all(motif in seq for seq in seqs):
+#                 return motif
+#     return ''
 
 
 if __name__ == '__main__':
     LCSMSolution()
-
--- problems/LCSM/test_LCSM.py
+++ problems/LCSM/test_LCSM.py
@@ -2,10 +2,18 @@ from problems.LCSM.LCSM import LCSMSolution
 from shared.testing_utils import solution_output
 
 
-test_input = ""
-expected_output = ""
+test_input = """>Rosalind_1
+GATTACA
+>Rosalind_2
+TAGACCA
+>Rosalind_3
+ATACA"""
+expected_output = "TA\n"
 
 
-# def test_LCSMSolution():
-#     assert solution_output(LCSMSolution, test_input) == expected_output
+def test_LCSMSolution():
+    assert solution_output(LCSMSolution, test_input) == expected_output
 
+
+def test_LCSMSolution_algorithm():
+    assert LCSMSolution.algorithm(['GATTACA', 'TAGACCA', 'ATACA']) == 'TA'
