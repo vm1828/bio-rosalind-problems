87eb09cac661fadc9f189c9de7bffbf56a508694
vm1828 <vm18281828@gmail.com>
2025-01-27 13:43:47 +0200

    Add LGIS

--- problems/LGIS/LGIS.py
+++ problems/LGIS/LGIS.py
@@ -6,18 +6,50 @@ from shared.solution import Solution
 class LGISSolution(Solution):
 
     @staticmethod
-    def algorithm(s: str):
-        """...
+    def algorithm(seq: list[int], increasing: bool = True) -> tuple[int, ...]:
+        """
+        Returns the longest increasing (default) or decreasing subsequence.
 
         Args:
-            s (str): ...
+            seq (List[int]): List of integers.
+            increasing (bool, optional): If False, returns a descending subsequence. Defaults to True.
 
         Returns:
-            ...: ...
+            Tuple[int, ...]: Longest increasing subsequence.
         """
-        ...
+        dp = [1] * len(seq)      # dp array
+        parent = [-1] * len(seq)  # to reconstruct the subsequence
+
+        for i in range(1, len(seq)):
+            for j in range(i):
+                check = seq[i] > seq[j] if increasing else seq[i] < seq[j]
+                if check and dp[i] < dp[j] + 1:
+                    dp[i] = dp[j] + 1
+                    parent[i] = j
+
+        max_idx = dp.index(max(dp))
+        longest_subseq = []
+        while max_idx != -1:
+            longest_subseq.append(seq[max_idx])
+            max_idx = parent[max_idx]
+
+        return tuple(longest_subseq[::-1])
+
+    def _parse(self) -> list[int]:
+        with open(self._input_file) as f:
+            next(f)
+            s = next(f)
+        return [int(i) for i in s.split()]
+
+    def _solve(self) -> str:
+        longest_incr_subseq = ' '.join(
+            map(str, self.algorithm(self._parsed_data))
+        )
+        longest_decr_subseq = ' '.join(
+            map(str, self.algorithm(self._parsed_data, increasing=False))
+        )
+        return f"{longest_incr_subseq}\n{longest_decr_subseq}"
 
 
 if __name__ == '__main__':
     LGISSolution()
-
--- problems/LGIS/test_LGIS.py
+++ problems/LGIS/test_LGIS.py
@@ -2,10 +2,11 @@ from problems.LGIS.LGIS import LGISSolution
 from shared.testing_utils import solution_output
 
 
-test_input = ""
-expected_output = ""
+test_input = """5
+5 1 4 2 3"""
+expected_output = """1 2 3
+5 4 2\n"""
 
 
-# def test_LGISSolution():
-#     assert solution_output(LGISSolution, test_input) == expected_output
-
+def test_LGISSolution():
+    assert solution_output(LGISSolution, test_input) == expected_output
